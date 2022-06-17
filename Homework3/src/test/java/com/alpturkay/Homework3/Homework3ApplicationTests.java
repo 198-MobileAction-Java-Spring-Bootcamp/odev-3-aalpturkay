e250a8afc4eb32007664193b89b11d560098b27f
Aykut Alp Turkay <aykutalpturkay@gmail.com>
2022-06-17 15:40:31 +0300

    Initialize project

---
+++ Homework3/src/test/java/com/alpturkay/Homework3/Homework3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpturkay.Homework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
